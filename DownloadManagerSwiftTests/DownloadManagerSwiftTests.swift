86e042d51e93337944f09b89bab411d691649f07
Salem Mohammed <salem.muhmed@gmail.com>
2018-12-11 00:37:47 +0800

    add download manager test cases

--- DownloadManagerSwiftTests/DownloadManagerSwiftTests.swift
+++ DownloadManagerSwiftTests/DownloadManagerSwiftTests.swift
@@ -7,27 +7,66 @@
 //
 
 import XCTest
+@testable import DownloadManagerSwift
 
 class DownloadManagerSwiftTests: XCTestCase {
-
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        Cacher.sharedInstance.updateCacheLimit(toLimit: 0)
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testDownloadAndCache() {
+        let imageUrl = "https://www.gettyimages.com/gi-resources/images/Embed/new/embed2.jpg"
+        let promise = expectation(description: "Image is downloaded and cached")
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        DownloadManager.loadImage(fromUrl: imageUrl) { (image, error) in
+            
+            if image != nil {
+                // Assert caching the image data
+                XCTAssertNotNil(Cacher.sharedInstance.fileData(forKey: imageUrl))
+                promise.fulfill()
+            }else {
+                XCTFail("Failed with error: \(String(describing: error?.localizedDescription))")
+            }
         }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testCacheCountLimit () {
+        
+        let smallImageUrl = "https://www.gettyimages.com/gi-resources/images/Embed/new/embed2.jpg"
+        let largeImageUrl = "https://wallpaperbrowse.com/media/images/3848765-wallpaper-images-download.jpg"
+   
+        Cacher.sharedInstance.updateCacheLimit(toLimit: 1)
+        
+        let promise = expectation(description: "Only 1 image is cached")
+        
+        DownloadManager.loadImage(fromUrl: smallImageUrl) { (image, error) in
+            
+            guard image != nil else {
+                XCTFail("Failed with error: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            
+            // Assert the small image is cached
+            XCTAssertNotNil(Cacher.sharedInstance.fileData(forKey: smallImageUrl))
+        }
+        
+        DownloadManager.loadImage(fromUrl: largeImageUrl) { (largeImage, error) in
+            guard largeImage != nil else {
+                XCTFail("Failed with error: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            // The small image is removed from the cache and the large image is saved
+            XCTAssertNil(Cacher.sharedInstance.fileData(forKey: smallImageUrl))
+            XCTAssertNotNil(Cacher.sharedInstance.fileData(forKey: largeImageUrl))
+            promise.fulfill()
+        }
+
+        waitForExpectations(timeout: 5, handler: nil)
     }
 
 }
